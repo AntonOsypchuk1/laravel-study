312ff82804662d0158046d46af1dd3cce22cedcc
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-06-26 19:55:42 +0300

    proj uploaded to git

---
+++ laragigs/routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// All Listings
+Route::get('/', [
+    ListingController::class, 'index'
+]);
+
+// Show Create Form
+Route::get('/listings/create', [
+    ListingController::class, 'create'
+])->middleware('auth');
+
+// Store Listing Data
+Route::post('/listings', [
+    ListingController::class, 'store'
+])->middleware('auth');
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [
+    ListingController::class, 'edit'
+])->middleware('auth');
+
+// Update Listing
+Route::put('/listings/{listing}', [
+    ListingController::class, 'update'
+])->middleware('auth');
+
+// Delete Listing
+Route::delete('/listings/{listing}', [
+    ListingController::class, 'delete'
+])->middleware('auth');
+
+// Manage Listings
+Route::get('/listings/manage', [
+    ListingController::class, 'manage'
+])->middleware('auth');
+
+// Single Listing
+Route::get('/listing/{listing}', [
+    ListingController::class, 'show'
+]);
+
+// Show Register/Create Form
+Route::get('/register', [
+    UserController::class, 'register'
+])->middleware('guest');
+
+// Register New User
+Route::post('/users', [
+    UserController::class, 'store'
+])->middleware('guest');
+
+// Log User Out
+Route::post('/logout', [
+    UserController::class, 'logout'
+])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [
+    UserController::class, 'login'
+])->name('login')->middleware('guest');
+
+// Log in User
+Route::post('/users/authenticate', [
+    UserController::class, 'authenticate'
+])->middleware('guest');
